347ae72f5d885a068a5d2395120636816c58afeb
東 俊光 <east@PISMONT.local>
2014-09-07 17:29:13 +0900

    Initial Commit

---
+++ PhysicsSimulation_22_FlagSwinging/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  PhysicsSimulation_22_FlagSwinging
+//
+//  Created by 東 俊光 on 2014/09/07.
+//  Copyright (c) 2014年 東 俊光. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
